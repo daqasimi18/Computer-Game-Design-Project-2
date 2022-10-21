0cdc42ae75f825e337c0d57bd6865117f48244f4
kszieba <toriazure@gmail.com>
2022-10-21 05:15:29 -0400

    Added collectibles, on-screen count, and win text
    I also adjusted the Y rotation of the Main Virtual Camera to 0 so the W key would match up with forward in relation to the camera.

--- Project-2/Assets/_Project Robotboy/Scripts/PlayerMovement.cs
+++ Project-2/Assets/_Project Robotboy/Scripts/PlayerMovement.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//borrowed from Roll-a-Ball tutorial
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -8,11 +10,23 @@ public class PlayerMovement : MonoBehaviour
 	public float rotationSpeed;
 
 	private CharacterController characterController;
+	//lines borrowed from Roll-a-Ball tutorial
+	private int count;
+	public GameObject CollectParent;
+	private int collectCount;
+	public TextMeshProUGUI countText;
+	public GameObject winTextObject;
 	
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+		
+		//lines borrowed from Roll-a-Ball tutorial
+		count = 0;
+		collectCount = CollectParent.transform.childCount;
+		SetCountText();
+		winTextObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -34,4 +48,25 @@ public class PlayerMovement : MonoBehaviour
 			transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
 		}
     }
+	
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString() + "/" + collectCount.ToString();
+		if (count>=collectCount)
+		{
+			winTextObject.SetActive(true);
+			//restartButton.SetActive(true);
+		}
+	}
+	
+	//from Roll-a-Ball tutorial
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Collect"))
+		{
+			other.gameObject.SetActive(false);
+			count += 1;
+			SetCountText();
+		}
+	}
 }
